5a9666af128b3b4842d248289722b81f191c0cbf
w1llow1sp <mari.front.74@gmail.com>
2023-03-09 18:35:33 +0500

    18 Lesson : Added Dialogs component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "./App.css";
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
+import Dialogs from "./components/Dialogs/Dialogs";
 
 
 
@@ -12,7 +13,10 @@ const App = () => {
         <div className='app-wrapper'>
             <Header/>
             <Navbar/>
-            <Profile/>
+            <div className='app-wrapper-content'>
+                {/* <Profile/>*/}
+                <Dialogs />
+            </div>
 
         </div>
     );
---
+++ src/components/Dialogs/Dialogs.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Dialogs.module.css'
+
+const Dialogs = () => {
+    return (
+        <div >
+            <h1>Dialogs</h1>
+        </div>
+    );
+};
+
+export default Dialogs;
