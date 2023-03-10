39010b2ee79421f157a49c8216ac62e81b1428c2
w1llow1sp <mari.front.74@gmail.com>
2023-03-10 13:40:24 +0500

    19 Lesson : Added some components for routing

---
+++ src/components/Music/Music.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Music.module.css'
+
+const Music = () => {
+    return (
+        <div>
+            <h1>Music</h1>
+        </div>
+    );
+};
+
+export default Music;
---
+++ src/components/News/News.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './News.module.css'
+
+const News = () => {
+    return (
+        <div>
+            <h1>News</h1>
+        </div>
+    );
+};
+
+export default News;
