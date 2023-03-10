0ce7070539a3c563a18af1567addcb22414a168d
w1llow1sp <mari.front.74@gmail.com>
2023-03-10 14:38:12 +0500

    21 Lesson : Modify dialogs components add some styles for it

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,8 +3,18 @@ import styles from './Dialogs.module.css'
 
 const Dialogs = () => {
     return (
-        <div >
-            <h1>Dialogs</h1>
+        <div className={styles.dialogs}>
+            <div className={styles.dialogsItem}>
+                <div className={`${styles.dialog} + ${styles.active}`}>Dima</div>
+                <div className={styles.dialog}>Valera</div>
+                <div className={styles.dialog}>Valentin</div>
+                <div className={styles.dialog}>Sasha</div>
+            </div>
+            <div className={styles.messages}>
+                <div className={styles.message}>Hi</div>
+                <div className={styles.message}>How are you?</div>
+                <div className={styles.message}>Yo!</div>
+            </div>
         </div>
     );
 };
