d4998d2b2c825e5ef8c493ae56b71cede6203451
w1llow1sp <mari.front.74@gmail.com>
2023-03-10 15:23:15 +0500

    22 Lesson : Modify dialogs components

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,14 +1,29 @@
 import React from 'react';
 import styles from './Dialogs.module.css'
+import {NavLink} from "react-router-dom";
 
 const Dialogs = () => {
     return (
         <div className={styles.dialogs}>
             <div className={styles.dialogsItem}>
-                <div className={`${styles.dialog} + ${styles.active}`}>Dima</div>
-                <div className={styles.dialog}>Valera</div>
-                <div className={styles.dialog}>Valentin</div>
-                <div className={styles.dialog}>Sasha</div>
+                <div className={`${styles.dialog} + ${styles.active}`}>
+                    <NavLink to={'/dialogs/1'}>Dima</NavLink>
+                </div>
+                <div className={styles.dialog}>
+                    <NavLink to={'/dialogs/2'}>Valera</NavLink>
+                </div>
+                <div className={styles.dialog}>
+                    <NavLink to={'/dialogs/3'}>Valentyn</NavLink>
+                </div>
+                <div className={styles.dialog}>
+                    <NavLink to={'/dialogs/4'}>Sasha</NavLink>
+                </div>
+                <div className={styles.dialog}>
+                    <NavLink to={'/dialogs/5'}>Zhenya</NavLink>
+                </div>
+                <div className={styles.dialog}>
+                    <NavLink to={'/dialogs/6'}>Dasha</NavLink>
+                </div>
             </div>
             <div className={styles.messages}>
                 <div className={styles.message}>Hi</div>
