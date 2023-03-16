65a9d0779a351f6c7220f3e6139f7a4742e9fc1a
w1llow1sp <mari.front.74@gmail.com>
2023-03-16 12:30:59 +0500

    24 Lesson : Removed data from props to separate arrays

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -2,17 +2,17 @@ import React from 'react';
 import styles from './Dialogs.module.css'
 import {NavLink} from "react-router-dom";
 
-export type DialogItemPropsType ={
-    id:number
-    name:string
+export type DialogItemPropsType = {
+    id: number
+    name: string
 }
 
-export type MesagePropsType ={
-    message:string
+export type MesagePropsType = {
+    message: string
 }
 
-const DialogItem =(props:DialogItemPropsType)=>{
-    let path = '/dialogs/'+props.id
+const DialogItem = (props: DialogItemPropsType) => {
+    let path = '/dialogs/' + props.id
     return (
         <div className={`${styles.dialog} + ${styles.active}`}>
             <NavLink to={path}>{props.name}</NavLink>
@@ -20,27 +20,76 @@ const DialogItem =(props:DialogItemPropsType)=>{
     )
 }
 
-const Message = (props:MesagePropsType) => {
+const Message = (props: MesagePropsType) => {
     return (
         <div className={styles.message}>{props.message}</div>
     )
 }
 
-const Dialogs = () => {
+const Dialogs = (props: DialogItemPropsType) => {
+    let dialogsData = [
+        {
+            id: 1,
+            name: "Dima"
+        }, {
+            id: 2,
+            name: "Sveta"
+        }, {
+            id: 3,
+            name: "Yaroslav"
+        }, {
+            id: 4,
+            name: "Kostya"
+        }, {
+            id: 5,
+            name: "Zhenya"
+        }, {
+            id: 6,
+            name: "Dariya"
+        }, {
+            id: 7,
+            name: "Nastya"
+        }, {
+            id: 8,
+            name: "Marina"
+        },
+
+    ]
+
+    let messageData = [
+        {
+            id: 1,
+            message: 'Hi. How are u?'
+        }, {
+            id: 2,
+            message: 'I want hangout w/ Sasha. That u thing?'
+        }, {
+            id: 3,
+            message: 'You havent been in street racing for a long time!'
+        }, {
+            id: 4,
+            message: 'I found a cool coffee shop. Are we going?'
+        }, {
+            id: 5,
+            message: 'How about a call on Discord?'
+        },
+    ]
     return (
         <div className={styles.dialogs}>
             <div className={styles.dialogsItem}>
-                <DialogItem name={'Dima'} id={1}/>
-                <DialogItem name={'Valera'} id={2}/>
-                <DialogItem name={'Valentyn'} id={3}/>
-                <DialogItem name={'Sasha'} id={4}/>
-                <DialogItem name={'Zhenya'} id={5}/>
-                <DialogItem name={'Dasha'} id={6}/>
+                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>
+                <DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>
+                <DialogItem name={dialogsData[2].name} id={dialogsData[2].id}/>
+                <DialogItem name={dialogsData[3].name} id={dialogsData[3].id}/>
+                <DialogItem name={dialogsData[4].name} id={dialogsData[4].id}/>
+                <DialogItem name={dialogsData[5].name} id={dialogsData[5].id}/>
             </div>
             <div className={styles.messages}>
-                <Message message={'Hi'}/>
-                <Message message={'How are you?'}/>
-                <Message message={'Yo!'}/>
+                <Message message={messageData[0].message}/>
+                <Message message={messageData[1].message}/>
+                <Message message={messageData[2].message}/>
+                <Message message={messageData[3].message}/>
+                <Message message={messageData[4].message}/>
             </div>
         </div>
     );
--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -2,7 +2,24 @@ import React from "react";
 import  styles from './MyPosts.module.css'
 import Post from "./Post/Post";
 
-const MyPosts =( )=> {
+export type postDataPropsType ={
+    id:number,
+    message:string,
+    likesCount:number
+}
+
+const MyPosts =()=> {
+    let postData = [
+        {
+            id: 1,
+            message: 'Its my first post!',
+            likesCount:12
+        }, {
+            id: 2,
+            message: 'I think to make some coffee..',
+            likesCount:23
+        },
+    ]
     return (
             <div className={styles.postsBlock}>
                 <h3>My posts</h3>
@@ -13,8 +30,8 @@ const MyPosts =( )=> {
 
                 </div>
                 <div className={styles.posts}>
-                    <Post message={'Heey, this is my first post!'} likesCount={23}/>
-                    <Post message={'I think to make some coffee..'} likesCount={3}/>
+                    <Post message={postData[0].message} likesCount={postData[0].likesCount}/>
+                    <Post message={postData[1].message} likesCount={postData[1].likesCount}/>
                 </div>
             </div>
 
