44426495ae4cc8f077287ecc6b235ba4306b3d43
w1llow1sp <mari.front.74@gmail.com>
2023-03-23 15:42:46 +0500

    31 Lesson : Added a function to get the input value and created a ref

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -8,14 +8,23 @@ import {ProfileProps} from "../../../types/types";
 const MyPosts = (props:ProfileProps) => {
     let postsElement = props.postsProps.map(post =>
         <Post key={post.id} message={post.message} likesCount={post.likesCount}/>)
+
+    let newPostElement = React.createRef<HTMLTextAreaElement>()
+
+    function addPost() {
+       let text = newPostElement.current?.value
+        alert(text)
+
+    }
+
     return (
         <div className={styles.postsBlock}>
             <h3>My posts</h3>
             <div>
-                <div><textarea></textarea></div>
+                <div><textarea ref={newPostElement}></textarea></div>
 
                 <div>
-                    <button>Add post</button>
+                    <button onClick={addPost}>Add post</button>
                 </div>
 
             </div>
